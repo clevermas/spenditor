78500d9356744fb28e55c5a8b473fe7e9920f219
clevermas <clevermas@gmail.com>
2023-12-08 22:52:39 +0200

    feat: added mobile navigation dialog

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,9 +1,19 @@
+"use client";
+
 import Sidebar from "@/components/sidebar/sidebar";
+import { cn } from "@/lib/utils";
+import { useAppSelector } from "@/redux/hooks";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const { isOpen } = useAppSelector((state) => state.sidebarReducer);
   return (
     <>
-      <div className="min-h-screen pl-[56px] lg:px-[280px] bg-neutral-50 dark:bg-background">
+      <div
+        className={cn(
+          "min-h-screen pt-12 lg:pt-0 xl:px-[280px] bg-neutral-50 dark:bg-background",
+          isOpen ? "lg:pl-[240px]" : "lg:pl-[56px]"
+        )}
+      >
         <Sidebar></Sidebar>
         <main>{children}</main>
       </div>
